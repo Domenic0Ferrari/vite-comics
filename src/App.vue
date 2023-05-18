d6f43f544a8a2a0eb903c32c71f9e5f20fa82433
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-18 14:40:49 +0200

    add first structure and import SASS

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import ComicHeader from './components/ComicHeader.vue'
+import ComicMain from './components/ComicMain.vue'
+import ComicFooter from './components/ComicFooter.vue'
+export default {
+  components: {
+    ComicHeader,
+    ComicMain,
+    ComicFooter
+  }
+}
+</script>
+
+<template>
+  <ComicHeader />
+  <ComicMain />
+  <ComicFooter />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
