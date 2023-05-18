a4318eee40c10acfafa9d078e49d3d1bed25ed1d
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-18 14:55:44 +0200

    add style in ComicHeader

--- src/components/ComicHeader.vue
+++ src/components/ComicHeader.vue
@@ -1,6 +1,79 @@
 <script>
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <div class="quadrato"></div>
+            <ul>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+                <li>
+                    <a href="#">CIAO</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    background-color: white;
+
+    .container {
+        max-width: 1000px;
+        height: 100px;
+        background-color: salmon;
+        margin-inline: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .quadrato {
+        height: 100%;
+        width: 100px;
+        background-color: gold;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        gap: .7rem;
+
+        a {
+            text-decoration: none;
+            color: #585350;
+
+            &:hover {
+                color: blue;
+            }
+        }
+    }
+}
+</style>
