e7c959d0cbe3b5264990b84f401c357b72b0107b
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-21 18:03:48 +0200

    add card with props

--- src/components/ComicMain.vue
+++ src/components/ComicMain.vue
@@ -1,28 +1,103 @@
 <script>
+import cardMain from './cardMain.vue';
+export default {
+    data() {
+        return {
+            arrComics: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
+        }
+    },
+    components: {
+
+        cardMain,
+    }
+}
 </script>
 
 <template>
     <div class="main_top">
         <img src="../assets/img/jumbotron.jpg" alt="jumbotron" class="jumbo">
-        <button class="series">CURRENT SERIES</button>
+        <button class="series">
+            <a href="#">CURRENT SERIES</a>
+        </button>
     </div>
     <div class="main_bottom">
         <div class="container">
             <ul>
                 <li>
-                    <a href="#"><span>DIGITAL COMICS</span></a>
-                </li>
-                <li>
-                    <a href="#"><span>DC MERCHANDISE</span></a>
-                </li>
-                <li>
-                    <a href="#"><span>SUBSCRIPTION</span></a>
-                </li>
-                <li>
-                    <a href="#"><span>COMIC SHOP LOCATOR</span></a>
-                </li>
-                <li>
-                    <a href="#"><span>DC POWER VISA</span></a>
+                    <cardMain v-for="comic in arrComics" :key="comic.series" :thumb="comic.thumb" :series="comic.series" />
                 </li>
             </ul>
         </div>
@@ -45,33 +120,32 @@
 
     .series {
         background-color: #0282F9;
-        color: white;
         font-size: 18px;
         padding: 0.5rem 1rem;
         border: 1px solid #0282F9;
         position: absolute;
         bottom: -20px;
-        left: 300px
+        left: 300px;
+
+        a {
+            color: white;
+        }
     }
 }
 
 .main_bottom {
-    height: 500px;
     background-color: #1C1C1C;
 
     .container {
         @include container_general;
+        padding-top: 2rem;
 
-        ul {
-            height: 100%;
+        li {
             display: flex;
-            align-items: center;
-            justify-content: space-around;
+            flex-wrap: wrap;
             gap: .5rem;
+            justify-content: space-between;
         }
-
     }
-
-
 }
 </style>
---
+++ src/components/cardMain.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        thumb: String,
+        series: String,
+
+    }
+};
+</script>
+
+<template>
+    <div class="card">
+        <img :src="thumb" :alt="series" class="img_card">
+        <a href="#">{{ series }}</a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    width: calc((100% / 6) - .5rem);
+
+    .img_card {
+        width: 100%;
+        height: 200px;
+    }
+
+    a {
+        color: white;
+    }
+}
+</style>
